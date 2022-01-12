167176e349293c007fb488350e4b6c6255f879fe
farinha <farinha@gmail.com>
2022-01-12 16:13:21 +0000

    First commit

---
+++ Controllers/DeviceController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc.Controllers
+{
+    public class DeviceController : Controller
+    {
+        public ActionResult Index()
+        {
+            var deviceList = new DeviceList
+            {
+                Devices = new List<Device>
+                {
+                    new Laptop { Kind = "Laptop", CPUIndex = "CPU-Index" },
+                    new SmartPhone { Kind = "SmartPhone", ScreenSize = "Screen Size" }
+                }
+            };
+
+            return View(deviceList);
+        }
+
+        [HttpPost]
+        public ActionResult Index(DeviceList deviceList)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Device.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc.Models
+{
+    public class DeviceList
+    {
+        public List<Device> Devices { get; set; }
+    }
+
+    public abstract class Device
+    {
+        public string Kind { get; set; }
+    }
+
+    public class Laptop : Device
+    {
+        public string CPUIndex { get; set; }
+    }
+
+    public class SmartPhone : Device
+    {
+        public string ScreenSize { get; set; }
+    }
+}
